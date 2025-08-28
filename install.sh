65d6c9697d0bcdf9b6945ca9222dc9544ee06261
Ebbe Wertz <ebbe.wertz8@gmail.com>
2025-08-28 15:06:24 +0200

    kleine aanpassing in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 read -p "Press any key to start deployment"
 
 # Variables
-SRC_DIR="/root/mc-dashboard-repo"           # where you cloned your repo
 TARGET_DIR="/home/minecraft/mc-roestbak-dashboard"
 BINARY_NAME="server-executable"
 
@@ -11,10 +10,10 @@ echo "Preparing target directory..."
 mkdir -p "$TARGET_DIR"
 
 echo "Copying binary..."
-cp "$SRC_DIR/build/mc-dashboard-linux" "$TARGET_DIR/$BINARY_NAME"
+cp "./build/dashboard-executable" "$TARGET_DIR/$BINARY_NAME"
 
 echo "Copying templates..."
-cp -r "$SRC_DIR/src/html" "$TARGET_DIR/html"
+cp -r "./src/html" "$TARGET_DIR/html"
 
 echo "Setting permissions..."
 chown -R minecraft:minecraft "$TARGET_DIR"
